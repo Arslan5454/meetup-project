0690859ce52608378631745ddb157fb9ac2ccf46
Arslan5454 <arslanhanif5454@gmail.com>
2018-09-15 22:54:22 -0700

    lastest updates

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -1,39 +1,36 @@
 <template>
-    <v-container>
-        <v-layout row wrap>
-            <v-flex xs12>
-                <v-card>
-                    <v-card-title>
-                     <h6 class="primary--text">My Meetup</h6>
-                    </v-card-title>
-                    <v-card-media
-                    src="https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg"
-                    height="400px">
-                    </v-card-media>
-                    <v-card-text>
-                        <div class="info--text">18th July 2019</div>
-                        <div>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                        Veritatis explicabo recusandae molestias officiis ex corrupti obcaecati iusto, 
-                        autem cum voluptates fuga voluptate suscipit at laboriosam? Itaque asperiores nemo nihil minus?
-                        </div>
-                    </v-card-text>
-                    <v-card-action>
-                        <v-spacer></v-spacer>
-                        <v-btn class="error">Register</v-btn>
-                    </v-card-action>
-                </v-card>
-            </v-flex>
-        </v-layout>
-    </v-container>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
+          <v-card-title>
+            <h3 class="error--text">{{ meetup.title }}</h3>
+          </v-card-title>
+          <v-img
+            :src="meetup.imageUrl"
+            height="400px"
+          ></v-img>
+          <v-card-text>
+            <div class="info--text">{{ meetup.date }} - {{ meetup.location }}</div>
+            <div>{{ meetup.description }}</div>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="primary">Register</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
 <script>
-export default {
-  props: ['id'],
-  computed: {
-    meetup () {
-      return this.$store.getters.loadedMeetups(this.id)
+  export default {
+    props: ['id'],
+    computed: {
+      meetup () {
+        return this.$store.getters.loadedMeetup(this.id)
+      }
     }
   }
-}
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -10,19 +10,17 @@ export const store = new Vuex.Store({
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg',
         id: 'afajfjadfaadfa323',
         title: 'Meetup in New York',
-        date: '2017-07-17'
+        date: '2017-07-17',
+        location: 'New York',
+        description: 'New York, New York!'
       },
       {
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/7/7a/Paris_-_Blick_vom_gro%C3%9Fen_Triumphbogen.jpg',
         id: 'aadsfhbkhlk1241',
         title: 'Meetup in Paris',
-        date: '2017-07-19'
-      },
-      {
-        imageUrl: 'https://images5.alphacoders.com/340/thumb-1920-340961.jpg',
-        id: 'aadsfhbkhlk1241',
-        title: 'Meetup in Londan',
-        date: '2017-07-20'
+        date: '2017-07-19',
+        location: 'Paris',
+        description: 'It\'s Paris!'
       }
     ],
     user: {
