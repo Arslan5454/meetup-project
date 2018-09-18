e47975a47939b8982495ef2f729cde93546d2a17
Arslan5454 <arslanhanif5454@gmail.com>
2018-09-18 03:38:19 -0700

    fetching users

--- src/main.js
+++ src/main.js
@@ -40,6 +40,7 @@ new Vue({
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.$store.dispatch('autoSignIn', user)
+        this.$store.dispatch('fetchUserData', user)
       }
     })
     this.$store.dispatch('loadMeetups')
